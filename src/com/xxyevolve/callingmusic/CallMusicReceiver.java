886f589d9b170afa26021e76c805158b027b096c
wangxuecheng <wangxuecheng@putaogame.com>
2016-04-12 18:42:40 +0800

    jj

--- src/com/xxyevolve/callingmusic/CallMusicReceiver.java
+++ src/com/xxyevolve/callingmusic/CallMusicReceiver.java
@@ -6,6 +6,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
@@ -13,6 +15,7 @@ import android.util.Log;
  * @author save
  *主要为了监听通话中的各种事件
  */
+
 public class CallMusicReceiver extends BroadcastReceiver {
 	private static final String TAG = "CallMusicReceiver";
 	private Context mContext;
